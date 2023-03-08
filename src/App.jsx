a63dd0b65b512940d924d3b00b5f1e94af52981f
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-03-08 18:50:14 +0300

    start PLP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from '../src/components/Header';
+import PLP from '../src/components/PLP';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <PLP />
+    </>
   );
 }
 
--- src/components/PLP.jsx
+++ src/components/PLP.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const PLP = () => {
   return (
-    <div>PLP</div>
+    <div className='container'>PLPPLPPLPPLPPLP</div>
   )
 }
 
