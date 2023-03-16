12db9a46cb40ceb437bc5c7c615fa52fe5fdc2a2
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-03-16 17:38:37 +0300

    create currency dropdown

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import arrow from '../assets/icons/arrow.svg';
 
 const Cart = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto mt-80'>
       <span className='cart-categoty'>CART</span>
       <hr className='divider'></hr>
 
@@ -18,10 +18,10 @@ const Cart = () => {
           <div>
             <div className='cart-size-color-price'>SIZE:</div>
             <div className='flex'>
-              <div className='size-boxes relative'>XS</div>
-              <div className='size-boxes relative'>S</div>
-              <div className='size-boxes relative'>M</div>
-              <div className='size-boxes relative'>L</div>
+              <div className='size-boxes relative'><b>XS</b></div>
+              <div className='size-boxes relative'><b>S</b></div>
+              <div className='size-boxes relative'><b>M</b></div>
+              <div className='size-boxes relative'><b>L</b></div>
             </div>
           </div>
           <div>
@@ -39,18 +39,24 @@ const Cart = () => {
             <div className='quant'>1</div>
             <div className='quant-box'>-</div>
           </div>
-          <div>
+          <div className='relative mx-auto'>
             <img src={image_2} alt="" className='cart-img' />
-          </div>
-          <div className='arrows-container'>
+            <div className='arrows-container'>
             <div><img src={arrow} alt="" className='arrow' /></div>
             <div><img src={arrow} alt="" className='arrow right-arrow' /></div>
           </div>
+          </div>
+
         </div>
       </div>
 
       <hr className='divider-1'></hr>
 
+      <div className='cart-tax cart-bottom'>Tax 21%: <b>$42.00</b></div>
+      <div className='cart-quantity cart-bottom'>Quantity: <b>3</b></div>
+      <div className='cart-total cart-bottom'>Total: <b>$200.00</b></div>
+
+      <a href='/' className='cart-order'>ORDER</a>
     </div>
   )
 }
--- src/components/MiniCart.jsx
+++ src/components/MiniCart.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from 'react';
+import image_2 from '../assets/images/ProductD.png';
+import arrow from '../assets/icons/arrow.svg';
 
 const MiniCart = () => {
   return (
-    <div>MiniCart</div>
+    <div className='mini-cart-container absolute border-black'>
+      <div className='mx-auto'>
+        <span className='minicart-categoty'><b>My Bag, </b>3 items</span>
+
+        <div className='minicart-container'>
+          <div className='left-container-minicart'>
+            <div className='minicart-title'>Apollo</div>
+            <div className='minicart-subtitle'>Running Shoes</div>
+            <div className='minicart-price'>$50.00</div>
+            <div>
+              <div className='minicart-size-color-price'>SIZE:</div>
+              <div className='flex'>
+                <div className='size-boxes-minicart relative'><b>XS</b></div>
+                <div className='size-boxes-minicart relative'><b>S</b></div>
+                <div className='size-boxes-minicart relative'><b>M</b></div>
+                <div className='size-boxes-minicart relative'><b>L</b></div>
+              </div>
+            </div>
+            <div>
+              <div className='minicart-size-color-price'>COLOR:</div>
+              <div className='flex'>
+                <div className='color-boxes-minicart'></div>
+                <div className='color-boxes-minicart'></div>
+                <div className='color-boxes-minicart'></div>
+              </div>
+            </div>
+          </div>
+          <div className='right-container-minicart'>
+            <div className='quantity-minicart-container'>
+              <div className='quant-box-minicart'>+</div>
+              <div className='quant-minicart'>1</div>
+              <div className='quant-box-minicart'>-</div>
+            </div>
+            <div>
+              <img src={image_2} alt="" className='minicart-img' />
+            </div>
+          </div>
+        </div>
+
+        <div className='minicart-total'>
+          <span><b>Total</b></span>
+          <span><b>$200.00</b></span>
+        </div>
+        <div className='minicart-checkout'>
+          <a href='/' className='viewbag'>VIEW BAG</a>
+          <a href='/' className='checkout'>CHECK OUT</a>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
