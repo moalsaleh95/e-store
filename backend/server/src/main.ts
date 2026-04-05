a6cc7b9409145ee3c0723dda21626b188096b64f
mo.alsaleh <mo.alsaleh95@gmail.com>
2026-04-05 20:04:41 +0200

    CORS possible issue fix

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -1,12 +1,17 @@
 import { ApolloServer } from 'apollo-server';
-
-
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
 const server = new ApolloServer({
     typeDefs: typeDefs,
-    resolvers: resolvers
+    resolvers: resolvers,
+    cors: {
+    origin: [
+      'https://e-store-ciox.vercel.app',
+      'http://localhost:3000'
+    ],
+    credentials: true,
+  },
 });
 
 server.listen().then(({ url }) => {
